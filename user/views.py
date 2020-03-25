98322639c07084abce866885e25213ea167c85c8
Rayhan Hossen <rayhanhossan74@gmail.com>
2020-03-25 21:02:08 +0600

    view change

--- user/views.py
+++ user/views.py
@@ -10,7 +10,6 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            # username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created!')
             return redirect('login')
     form = UserRegisterForm()
